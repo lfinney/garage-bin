73491d0119dc78f1bf9d7c82f59fa0157f308448
Luke Finney <lucaswfinney@gmail.com>
2017-12-18 12:07:21 -0700

    Write GET function for frontend

--- public/scripts.js
+++ public/scripts.js
@@ -9,4 +9,20 @@ const createNewItem = (event) => {
   console.log(newItem);
 }
 
+const appendItems = (items) => {
+  console.log(items);
+}
+
+const fetchItems = () => {
+  fetch(`/api/v1/garageItems`)
+    .then(response => response.json())
+    .then(parsedItems => appendItems(parsedItems))
+    .catch(error => console.error(error))
+}
+
+const pageLoad = () => {
+  fetchItems();
+}
+
+$(document).ready(pageLoad);
 $('#submit-item').on('click', (event) => createNewItem(event))
