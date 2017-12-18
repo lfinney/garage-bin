4a7f288eb2ec2ca52b68bf18656a569d41b132b8
Luke Finney <lucaswfinney@gmail.com>
2017-12-18 13:23:48 -0700

    Display item counts to DOM

--- public/scripts.js
+++ public/scripts.js
@@ -20,15 +20,23 @@ const appendItems = (items) => {
   items.forEach((item) => {
     // cleanlinessSelection = selectOption(item.cleanliness);
     $(`#item-list`).append(`
-      <div class="item">
+      <div id="item-${item.id}" class="item">
         <h2 class="name">${item.name}</h2>
       </div>
     `)
   })
 }
 
+const filterType = (items, type) => {
+  const filteredItems = items.filter(item => item.cleanliness === type);
+  return filteredItems.length;
+}
+
 const appendCount = (parsedItems) => {
-  $('#item-count').text(parsedItems.length);
+  $('#total-items').text(`${parsedItems.length} - Total Items`);
+  $('#total-sparkling').text(`${filterType(parsedItems, 'Sparkling')} - Sparkling`)
+  $('#total-dusty').text(`${filterType(parsedItems, 'Dusty')} - Dusty`)
+  $('#total-rancid').text(`${filterType(parsedItems, 'Rancid')} - Rancid`)
 }
 
 const fetchItems = () => {
