dbc30bdcb52ba137daac95219ba1b0923e27fc8e
Luke Finney <lucaswfinney@gmail.com>
2017-12-18 09:29:28 -0700

    create initial database table

--- db/migrations/20171218092032_initial.js
+++ db/migrations/20171218092032_initial.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('garage_items', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('reason');
+      table.string('cleanliness');
+
+      table.timestamps(true, true);
+    }),
+
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+      knex.schema.dropTable('garage_items')
+    ]);
 };
