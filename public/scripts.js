875eb0803333f128e2dbf81c260e887708afe418
Luke Finney <lucaswfinney@gmail.com>
2017-12-18 13:05:28 -0700

    Display all items in garage on DOM and display number

--- public/scripts.js
+++ public/scripts.js
@@ -9,14 +9,35 @@ const createNewItem = (event) => {
   console.log(newItem);
 }
 
+// const selectOption = (cleanliness) => {
+//   let cleanlinessOptions = ['Sparkling', 'Dusty', 'Rancid']
+//   cleanlinessOptions = cleanlinessOptions.filter(option => option !== cleanliness);
+//   console.log(cleanlinessOptions);
+//   return cleanlinessOptions.unshift(cleanliness);
+// }
+
 const appendItems = (items) => {
-  console.log(items);
+  items.forEach((item) => {
+    // cleanlinessSelection = selectOption(item.cleanliness);
+    $(`#item-list`).append(`
+      <div class="item">
+        <h2 class="name">${item.name}</h2>
+      </div>
+    `)
+  })
+}
+
+const appendCount = (parsedItems) => {
+  $('#item-count').text(parsedItems.length);
 }
 
 const fetchItems = () => {
   fetch(`/api/v1/garageItems`)
     .then(response => response.json())
-    .then(parsedItems => appendItems(parsedItems))
+    .then(parsedItems => {
+      appendCount(parsedItems)
+      appendItems(parsedItems)
+    })
     .catch(error => console.error(error))
 }
 
