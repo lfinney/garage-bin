1f301d2b92ab155fe2cf773b47d24f7617d11925
Luke Finney <lucaswfinney@gmail.com>
2017-12-18 10:08:15 -0700

    Write GET endpoints

--- server.js
+++ server.js
@@ -2,14 +2,41 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Garage Bin';
 
+
 app.get('/', (request, response) => {
-  response.send('Whodis?');
+  response.send('Oh, hai!');
+});
+
+app.get('/api/v1/items', (request, response) => {
+  database('garage_items').select()
+    .then((items) => {
+      return response.status(200).json(items);
+    })
+    .catch((error) => {
+      return response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/items/:id', (request, response) => {
+  const { id } = request.params;
+
+  database('garage_items').where('id', id).select()
+    .then((items) => {
+      return response.status(200).json(items);
+    })
+    .catch((error) => {
+      return response.status(500).json({ error });
+    });
 });
 
 app.listen(app.get('port'), () => {
